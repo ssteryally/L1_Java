11f350d7a0e7939a8167a636d7dc4a1a4392127e
Denis <122615902+ssteryally@users.noreply.github.com>
2023-09-12 15:29:09 +0300

    Add files via upload

---
+++ control_lesson1/src/main/java/ru/mirea/shunyaev/control_lesson1/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.mirea.shunyaev.control_lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    //TextView tv = findViewById(R.id.editTextTextPersonName4);
+
+}
